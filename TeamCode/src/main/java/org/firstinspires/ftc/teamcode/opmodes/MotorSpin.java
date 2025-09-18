2a152e2353dc6531571af852e3c29f6e5e94ad05
Wanni <153240794+17-Wanni@users.noreply.github.com>
2025-09-18 16:36:13 -0600

    Create MotorSpin.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MotorSpin.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class MotorSpin extends OpMode {
+
+    DcMotor motor;
+
+
+    @Override
+    public void init() {
+        motor = hardwareMap.get(DcMotor.class, "motor");
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    @Override
+    public void loop() {
+        motor.setPower(0.7);
+    }
+
+}
+
+
+
