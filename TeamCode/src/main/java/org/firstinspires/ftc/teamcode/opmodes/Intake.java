721ad5a48a552def1463caa330186afdf93a2a7a
Toastiegg <Djwleong2@gmail.com>
2025-10-02 18:18:08 -0600

    Intake
    My name is Edwin
    I made the Intake
    it was difficult, to the put the pieces together.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Intake.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "intake")
+public class Intake extends OpMode {
+
+    CRServo LeftServo;
+
+    CRServo RightServo;
+
+    @Override
+    public void init() {
+
+        LeftServo = hardwareMap.get(CRServo.class, "LeftServo");
+        RightServo = hardwareMap.get(CRServo.class, "RightServo");
+
+
+    }
+
+    public void in(double power) {
+
+        LeftServo.setPower(power);
+        RightServo.setPower(-power);
+
+
+
+
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.right_bumper){
+
+                in(1);
+            }
+
+            if(gamepad1.left_bumper) {
+
+                in(0);
+    }
+
+    }
+
+
+
+
+
+}
